7777a26e606fcd1c8516242c1b291d088a684199
faturita <rramele@gmail.com>
2019-08-29 18:46:15 -0300

    Added code to handle the wakeup call from the system service

---
+++ NeoCortex/WakeUp.py
@@ -0,0 +1,14 @@
+import sys
+import os
+ 
+original = sys.stdout
+out = open('./console.log', 'w')
+sys.stdout = out
+sys.stderr = out
+print('Init')
+#sys.stdout = original
+
+if (os.access("running.wt", os.W_OK)):
+    os.remove("running.wt")
+
+import Brainstem
